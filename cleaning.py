74ab41b6dd828c90ecd4b221091fd067637805b9
Divine <divinelavente@gmail.com>
2023-11-29 16:00:40 +0800

    updated spam data
    >included dataset found in internet

--- cleaning.py
+++ cleaning.py
@@ -1,18 +1,31 @@
 import openpyxl
+import random
 
-book = openpyxl.load_workbook("responses.xlsx")
+book = openpyxl.load_workbook("spam data.xlsx")
 
 sheet = book.active
 
 for row in sheet.rows:
+    # if(row[2].value != None):                   #for cleaning numbers
+    #     a = str(row[2].value)
+    #     b = "+" + a[0:4] + "***" + a[7:]
+    #     row[2].value = b
+
     # if(row[1].value != None):                   #for cleaning numbers
     #     a = str(row[1].value)
-    #     b = "+" + a[0:4] + "***" + a[7:]
-    #     row[1].value = b
+    #     if a[0:2] == "++":
+    #         b = a[1:]
+    #         row[1].value = b
+
+    # if(row[1].value != None):                   #for cleaning numbers
+    #     a = str(row[1].value)
+    #     if(a[4:8] == "****"):
+    #         b = a[0:4] + str(random.randint(0, 9)) + "***" + a[8:]
+    #         row[1].value = b
 
     if(row[2].value != None):
         c = row[2].value
-        names = ["ANN BEATRICE D.", "KRIZTAL HOPE T.", "KURTNEY U.", "<recipient name>", "DARAH VIA D. M.", "GWYNETH ANNE L.","Jhon N.","JUNEVI B.","ANNE BEATRICE D.","KYLE E.","ARMEL SIEAN E.","JUSTINE C.", "JO-ANN J.","MICHAEL PATRICK P.","MICHAEL PATRICK P","RUSSEL JADE T.","JOSE MIGUEL G.", "FENNIE MAE T.","DIVINE GRACE L."]
+        names = ["<REAL NAME>", "ANN BEATRICE D.", "KRIZTAL HOPE T.", "KURTNEY U.", "<recipient name>", "DARAH VIA D. M.", "GWYNETH ANNE L.","Jhon N.","JUNEVI B.","ANNE BEATRICE D.","KYLE E.","ARMEL SIEAN E.","JUSTINE C.", "JO-ANN J.","MICHAEL PATRICK P.","MICHAEL PATRICK P","RUSSEL JADE T.","JOSE MIGUEL G.", "FENNIE MAE T.","DIVINE GRACE L."]
         
         for name in names:
             if name in c:
@@ -20,7 +33,7 @@ for row in sheet.rows:
                 row[2].value = d
                 break
 
-    # if(row[1].value != None):                               #to remove whitespace in numbers                                       
-    #     row[1].value = str(row[1].value).replace(' ','')
+    # if(row[2].value != None):                               #to remove whitespace in numbers                                       
+    #     row[2].value = str(row[2].value).replace(' ','')
 
-book.save('responses.xlsx')
+book.save('spam data.xlsx')
